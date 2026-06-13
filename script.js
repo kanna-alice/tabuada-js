fd2c27dc3afb93956c50fd14dda17d4b905432d7
Wallyson <wallysonm51@gmail.com>
2026-06-13 14:50:44 -0300

    Enhance tabuada function with input validation
    Added user input validation for number entry.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function tabuada(){
+    // Obtendo o número digitado pelo usuário
     let num = document.getElementById("num").value
     let res = document.getElementById("res")
     res.innerHTML = ""
     if (num == ""){
         alert("Digite um número!")
-    return
-    }
+    return}
+// Gerar a tabuada do número digitado
     else {
 for (let i = 1; i <= 10; i++){
     let item = document.createElement("option")
